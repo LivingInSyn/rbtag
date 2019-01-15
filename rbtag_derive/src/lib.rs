ad1ff81e951916f881b36d7f7bab77f97165cb0f
Jeremy Mill <jeremy.mill@otis.com>
2019-01-15 16:22:03 -0500

    changing output to: commit-timestamp-repo status'

--- rbtag_derive/src/lib.rs
+++ rbtag_derive/src/lib.rs
@@ -10,6 +10,7 @@ use std::process::Command;
 /// `&'static str`
 #[proc_macro_derive(BuildDateTime)]
 pub fn build_dt(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    println!("build dt called");
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
     let utc: String = Utc::now().to_rfc3339();
@@ -27,28 +28,59 @@ pub fn build_dt(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
 /// or *nix and returns it as a `&'static str`
 #[proc_macro_derive(BuildGitCommit)]
 pub fn get_build_commit(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    //git rev-parse --short HEAD
-    let git_command = "git rev-parse --short HEAD";
-    let output = if cfg!(target_os = "windows") {
+    println!("build commit called");
+    
+    //clean vs dirty command:
+    //git diff-index --quiet HEAD --
+    //0 if clean, 1+ if dirty
+
+    //git info command
+    //git show -s --format=%h-%ct
+    //output: fb77f93-1547517292
+
+    let git_commit_command = "git show -s --format=%h-%ct";
+    let git_dirty_command = "git diff-index --quiet HEAD --";
+    // get the git commit/datetime info
+    let commit_output = if cfg!(target_os = "windows") {
         Command::new("cmd")
-                .args(&["/C", git_command])
+                .args(&["/C", git_commit_command])
                 .output()
                 .expect("failed to execute process")
     } else {
         Command::new("sh")
                 .arg("-c")
-                .arg(git_command)
+                .arg(git_commit_command)
                 .output()
                 .expect("failed to execute process")
     };
+    // get the dirty status
+    let dirty_output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+                .args(&["/C", git_dirty_command])
+                .status()
+                .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+                .arg("-c")
+                .arg(git_dirty_command)
+                .status()
+                .expect("failed to execute process")
+    };
+    let dirty_string = match dirty_output.success() {
+        true => "clean",
+        false => "dirty"
+    };
+
+    let output = format!("{}-{}", String::from_utf8_lossy(&commit_output.stdout), dirty_string);
+
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
-    let stdout = String::from_utf8_lossy(&output.stdout);
+    //let stdout = String::from_utf8_lossy(&output.stdout);
     let expanded = quote! {
         impl BuildGitCommit for #name {
             fn get_build_commit(&self) -> &'static str {
                 //String::from("test")
-                #stdout
+                #output
             }
         }
     };
--- rbtag_example/src/main.rs
+++ rbtag_example/src/main.rs
@@ -6,4 +6,7 @@ struct BuildTag;
 fn main() {
     println!("{}", BuildTag{}.get_build_timestamp());
     println!("{}", BuildTag{}.get_build_commit());
+    
+    //println!("{}", rbtag::get_build_timestamp());
+    println!("{}", rbtag::get_build_commit());
 }
